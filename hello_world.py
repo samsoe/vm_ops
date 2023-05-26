6cc298c5378394564df91326e093aa9ab072b6ee
samsoe <esamsoe@gmail.com>
2023-05-26 11:49:40 -0600

    Initial Commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# Open the file in write mode
+file = open("/usr/local/bin/dataset/hello_world.txt", "w")
+
+# Write the text into the file
+file.write("Hello, world!")
+
+# Close the file
+file.close()
+
+# Print a message to indicate the file creation
+print("File 'hello_world.txt' created successfully.")
